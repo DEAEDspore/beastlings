78268c258baffb888331838f4b3088752d61dfd0
Андрей Дихтяренко <andrejdihtarenko@MacBook-Pro-Andrej.local>
2024-11-22 14:38:00 -0800

    start

---
+++ bot/bot.py
@@ -0,0 +1,36 @@
+import logging
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+# Токен твоего бота
+TOKEN = '7745016365:AAEzYu3H7IhUOv0NcdZkIr2qf5eNC6T9JVc'
+
+# Ссылка на сайт (игру)
+SITE_URL = "https://DEAEDspore.github.io/beastlings/web/"  # Актуальная ссылка на сайт
+
+# Функция для команды /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Создание кнопки с параметром web_app для мини-приложения
+    keyboard = [
+        [InlineKeyboardButton("Перейти в игру", web_app={'url': SITE_URL})]  # Используем web_app вместо url
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    # Отправка сообщения с кнопкой
+    await update.message.reply_text("Привет! Нажми кнопку ниже, чтобы перейти в игру:", reply_markup=reply_markup)
+
+def main():
+    # Настройка логирования
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    # Инициализация Application
+    application = Application.builder().token(TOKEN).build()
+
+    # Добавляем обработчик команды /start
+    application.add_handler(CommandHandler('start', start))
+
+    # Запуск бота
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
