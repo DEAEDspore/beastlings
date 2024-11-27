33af7f14cd63fc7cd17a2c43e95a545d2839c103
Андрей Дихтяренко <andrejdihtarenko@MacBook-Pro-Andrej.local>
2024-11-27 10:22:48 -0800

    added game interface

--- server.py
+++ server.py
@@ -1,14 +1,39 @@
 import os
-from flask import Flask
+import random
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello from Beastlings Server!"
+def index():
+    # Возвращаем HTML-файл
+    return render_template('index.html')
+
+@app.route('/play')
+def play():
+    # Получаем выбор игрока
+    player_choice = request.args.get('choice')
+    # Возможные варианты
+    choices = ['rock', 'paper', 'scissors']
+    bot_choice = random.choice(choices)
+    
+    # Логика игры
+    if player_choice == bot_choice:
+        result = "It's a draw!"
+    elif (player_choice == 'rock' and bot_choice == 'scissors') or \
+         (player_choice == 'scissors' and bot_choice == 'paper') or \
+         (player_choice == 'paper' and bot_choice == 'rock'):
+        result = "You win!"
+    else:
+        result = "You lose!"
+
+    # Возвращаем результат в формате JSON
+    return jsonify({
+        'player_choice': player_choice,
+        'bot_choice': bot_choice,
+        'result': result
+    })
 
 if __name__ == '__main__':
-    # Получение порта из переменной окружения или использование порта по умолчанию 5000
     port = int(os.environ.get('PORT', 5000))
-    # Установка '0.0.0.0' для привязки ко всем интерфейсам
     app.run(host='0.0.0.0', port=port, debug=True)
