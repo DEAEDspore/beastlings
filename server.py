243c88004ea21d6ebbd408a5aca0318f1881fa00
Андрей Дихтяренко <andrejdihtarenko@MacBook-Pro-Andrej.local>
2024-11-27 19:29:58 -0800

    beastlings png

--- server.py
+++ server.py
@@ -1,48 +1,15 @@
 import os
-import random
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Возвращаем HTML-файл
     return render_template('index.html')
-
-@app.route('/play')
-def play():
-    # Получаем выбор игрока
-    player_choice = request.args.get('choice')
-    # Возможные варианты
-    choices = ['rock', 'paper', 'scissors']
-    bot_choice = random.choice(choices)
-    
-    # Логика игры
-    if player_choice == bot_choice:
-        result = "It's a draw!"
-    elif (player_choice == 'rock' and bot_choice == 'scissors') or \
-         (player_choice == 'scissors' and bot_choice == 'paper') or \
-         (player_choice == 'paper' and bot_choice == 'rock'):
-        result = "You win!"
-    else:
-        result = "You lose!"
-
-    # Возвращаем результат в формате JSON
-    return jsonify({
-        'player_choice': player_choice,
-        'bot_choice': bot_choice,
-        'result': result
-    })
-
-@app.route('/loading')
-def loading_screen():
-    # Возвращаем HTML-файл для загрузочного экрана
-    return render_template('loading.html')
 
 @app.route('/choose')
 def choose():
     return render_template('choose.html')
-
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
