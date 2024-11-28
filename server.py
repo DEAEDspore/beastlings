e879e9941d378cbb1da2d861ef9eb3031266df5d
Андрей Дихтяренко <andrejdihtarenko@MacBook-Pro-Andrej.local>
2024-11-27 19:49:20 -0800

    loading

--- server.py
+++ server.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/loading')
+def loading():
     return render_template('index.html')
 
 @app.route('/choose')
