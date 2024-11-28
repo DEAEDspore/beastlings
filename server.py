8d2ef8815bf77c1e8f7c8386eaf921e1608b1b2d
Андрей Дихтяренко <andrejdihtarenko@MacBook-Pro-Andrej.local>
2024-11-27 19:36:50 -0800

    dmg

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -12,5 +11,4 @@ def choose():
     return render_template('choose.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
